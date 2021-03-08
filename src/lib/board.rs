3d5535290c58b71c5e2b695ed547c0a00b4e93fc
Guido Fioravantti <guido.fioravantti@gmail.com>
2021-03-07 21:45:39 -0500

    displaying something

--- src/lib/board.rs
+++ src/lib/board.rs
@@ -1,9 +1,18 @@
 pub struct Board {
-    positions: Vec<u16>,
+    pub positions: Vec<u16>,
 }
 
 pub fn init_board() -> Board {
     Board {
         positions: vec![0; 16],
     }
+}
+
+pub fn get_rows(board: &Board) -> Vec<Vec<u16>> {
+    vec!(
+        (&board.positions[0..4]).to_vec(),
+        (&board.positions[4..8]).to_vec(),
+        (&board.positions[8..12]).to_vec(),
+        (&board.positions[12..16]).to_vec(),
+    )
 }
--- src/lib/draw.rs
+++ src/lib/draw.rs
@@ -1,20 +1,53 @@
-use tui::Terminal;
-use tui::widgets::{Block, Borders};
-use termion::raw::RawTerminal;
-use tui::backend::TermionBackend;
 use std::io::Stdout;
 use super::board;
-
+use termion::raw::RawTerminal;
+use tui::Terminal;
+use tui::backend::TermionBackend;
+use tui::layout::Constraint;
+use tui::layout::Layout;
+use tui::style::Color;
+use tui::style::Style;
+use tui::widgets::Block;
+use tui::widgets::Borders;
+use tui::widgets::Cell;
+use tui::widgets::Row;
+use tui::widgets::Table;
+use tui::widgets::TableState;
 
 pub fn draw(
     terminal: &mut Terminal<TermionBackend<RawTerminal<Stdout>>>,
     board: &board::Board,
 ) {
+    let mut state = TableState::default();
+
     let _ = terminal.draw(|f| {
-        let size = f.size();
-        let block = Block::default()
-            .title("Block")
-            .borders(Borders::ALL);
-        f.render_widget(block, size);
+        let rects = Layout::default()
+            .constraints([Constraint::Percentage(100)].as_ref())
+            .margin(5)
+            .split(f.size());
+
+        let rows: Vec<Row> = board::get_rows(board)
+            .iter()
+            .map(|row| {
+                Row::new(
+                    row.iter().map(|n| {
+                        Cell::from(n.to_string())
+                    })
+                )
+                    .height(10 as u16)
+
+            })
+            .collect();
+
+        let table = Table::new(rows)
+            .block(Block::default().borders(Borders::ALL).title("Rusty 2048"))
+            .style(Style::default().bg(Color::Black))
+            .widths(&[
+                Constraint::Percentage(33),
+                Constraint::Length(30),
+                Constraint::Max(10),
+            ]);
+
+        f.render_stateful_widget(table, rects[0], &mut state);
     });
 }
